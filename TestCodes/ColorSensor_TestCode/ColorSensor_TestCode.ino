55f8f66c5641a395aca64dd6cd10951283bf3979
KennyNeutron <kennyneutron.hex@gmail.com>
2025-02-16 14:03:46 +0800

    added test code for color sensor but not verified with hardware yet

---
+++ TestCodes/ColorSensor_TestCode/ColorSensor_TestCode.ino
@@ -0,0 +1,50 @@
+#define ColorSensor_S0 4
+#define ColorSensor_S1 5
+#define ColorSensor_S2 7
+#define ColorSensor_S3 6
+
+#define ColorSensor_SignalPin 8
+
+int Filter_RED = 0;
+int Filter_GREEN = 0;
+int Filter_BLUE = 0;
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("ColorSensor Start");
+  ColorSensor_init();
+}
+
+
+void loop() {
+  Filter_RED = getColorFrequency(0, 0);
+  Filter_GREEN = getColorFrequency(1, 1);
+  Filter_BLUE = getColorFrequency(0, 1);
+
+  Serial.println("**************************");
+  Serial.println("RED Filter: " + String(Filter_RED));
+  Serial.println("GREEN Filter: " + String(Filter_GREEN));
+  Serial.println("BLUE Filter: " + String(Filter_BLUE));
+
+  delay(500);
+}
+
+
+void ColorSensor_init() {
+  pinMode(ColorSensor_S0, OUTPUT);
+  pinMode(ColorSensor_S1, OUTPUT);
+  pinMode(ColorSensor_S2, OUTPUT);
+  pinMode(ColorSensor_S3, OUTPUT);
+  pinMode(ColorSensor_SignalPin, INPUT);
+
+  digitalWrite(ColorSensor_S0, HIGH);
+  digitalWrite(ColorSensor_S1, LOW);
+}
+
+int getColorFrequency(bool s2_state, bool s3_state) {
+  digitalWrite(ColorSensor_S2, s2_state);
+  digitalWrite(ColorSensor_S3, s3_state);
+  delay(100);
+  pulseIn(ColorSensor_SignalPin, LOW);
+}
